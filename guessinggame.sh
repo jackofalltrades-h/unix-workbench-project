1cbc9af7e5fc52651973c24983b8c42a5c395637
pilotoj <joaopiloto22@hotmail.com>
2020-08-29 03:17:40 +0100

    added guessinggame.sh

---
+++ guessinggame.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+#File: guessinggame.sh
+filenum=$(ls | wc -l)
+
+function guess_filenum {
+        read guess
+        if [[ $guess -eq $filenum ]]
+        then
+                echo "Congratulations!! That's the right answer!!"
+
+                for f in $(ls)
+                do
+                        echo " $f "
+                done
+                echo "    ...bye now"
+        else
+                if [[ $guess -gt $filenum ]]
+                then
+                        echo "Oops, that's the wrong answer. There are more files than that, please try again and press Enter :"
+                        guess_filenum
+                else
+                        echo "Nope, that's incorrect, the number of files is higher than that. Please try again and press Enter :"
+                        guess_filenum
+                fi
+        fi
+}
+echo "This is the guessing game! Guess how many files are in the current directory!"
+guess_filenum
